aab0355524f47de5722e38a2470a17f23de7012b
Moses-SMusoki <109148109+Moses-SMusoki@users.noreply.github.com>
2023-03-18 09:57:44 +0300

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <section className="footer">
+    <div>
+      <p>© 2023 Altschool Sem 3 Exam</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
